a30a97b16dcc897c5224938deee3c00b022de9e1
Klbk <k13lbm@gmail.com>
2022-04-18 23:21:38 +0200

    New sound file

--- PimpuTimer/Properties/AssemblyInfo.cs
+++ PimpuTimer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PimpuTimer")]
+[assembly: AssemblyTitle("Czasomierz")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PimpuTimer")]
+[assembly: AssemblyCompany("MK")]
+[assembly: AssemblyProduct("Czasomierz")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
